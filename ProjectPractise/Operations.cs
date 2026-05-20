b207eb6727c86ee5737f75b4bc8974570da8681d
Arushi Tandon <arushitandon007@gmail.com>
2026-05-20 16:29:44 +0530

    Add project files.

---
+++ ProjectPractise/Operations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractise
+{
+    internal class Operations
+    {
+        int a, b, c;
+
+       internal void Accept()
+        {
+            Console.WriteLine("Enter First Number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        internal void Add()
+        {
+            c = a + b;
+            Console.WriteLine(c);
+        }
+       
+       internal void Subtract()
+        {
+            c = a - b;
+            Console.WriteLine(c);
+        }
+
+        internal void Multiply()
+        {
+            c = a * b;
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ ProjectPractise/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectPractise
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Operations obj = new Operations();
+            obj.Accept();
+            obj.Add();
+           
+        }
+    }
+}
