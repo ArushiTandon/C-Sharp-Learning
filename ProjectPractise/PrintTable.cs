2e01c5b8010a937657c2b5821af851dda45a39fa
Arushi Tandon <arushitandon007@gmail.com>
2026-05-22 20:28:52 +0530

    Print Table using for loop

---
+++ ProjectPractise/PrintTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractise
+{
+    internal class PrintTable
+    {
+        public void Print(int num)
+        {
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} * {i} = {num * i}");
+            }
+        }
+    }
+}
--- ProjectPractise/Program.cs
+++ ProjectPractise/Program.cs
@@ -5,8 +5,12 @@
        
         static void Main(string[] args)
         {
-            GreatestNumber obj = new GreatestNumber();
-            obj.CheckGreatest();
+            Console.WriteLine("Enter a number to print its multiplication table:");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            PrintTable obj = new PrintTable();
+            obj.Print(number);
 
 
         }
