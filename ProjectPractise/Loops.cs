b31975f273ed13f6d1d43bd6e4b002499e835ec4
Arushi Tandon <arushitandon007@gmail.com>
2026-05-24 18:05:53 +0530

    Inverted Character Pattern

--- ProjectPractise/Loops.cs
+++ ProjectPractise/Loops.cs
@@ -9,11 +9,13 @@ namespace ProjectPractise
         public void Pattern()
         {
 
-            for ( int i = 1; i <= 5; i++)
+            for ( int i = 5; i >= 0; i--)
             {
+                char ch = 'A';
+
                 for (int j = 1; j <= i; j++)
                 {
-                    Console.Write("*");
+                    Console.Write(ch++);
                 }
                 Console.WriteLine();
             }
