7014c503872a66897bde26339952723fee160464
Arushi Tandon <arushitandon007@gmail.com>
2026-05-27 17:36:10 +0530

    Programme

--- ProjectPractise/Program.cs
+++ ProjectPractise/Program.cs
@@ -1,16 +1,39 @@
-﻿namespace ProjectPractise
+﻿using System.Security.Cryptography;
+
+namespace ProjectPractise
 {
     internal class Program
     {
        
         static void Main(string[] args)
         {
-            Console.WriteLine(StaticExample.x);
-            StaticExample.fun();
+          
+           Student[] obj = new Student[3];
+            for(int i = 0; i < obj.Length; i ++)
+            {
+                obj[i] = new Student();
+                Console.WriteLine("Enter the roll number of student " );
+                int r_no = Convert.ToInt32(Console.ReadLine());
 
-            InstanceExample obj = new InstanceExample();
-            Console.WriteLine(obj.x);
-            obj.fun1();
+                Console.WriteLine("Enter the fees of student ");
+                int fees = Convert.ToInt32(Console.ReadLine());
+
+                obj[i].Accept(r_no , fees);
+            }
+
+            int max = 0;
+            int index = 0;
+            for( int i = 0; i < obj.Length; i++)
+            {
+                if (max < obj[i].fees)
+                {
+                    max = obj[i].fees;
+                    index = i;
+                }
+            }
+
+            Console.WriteLine("maximum fees is {0}", max);
+            obj[index].Display();
 
 
         }
---
+++ ProjectPractise/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractise
+{
+    internal class Student
+    {
+        internal int roll_no;
+        internal int fees;
+
+    internal void Accept(int r_no, int fees)
+        {
+            this.roll_no = r_no;
+            this.fees = fees;
+
+        }
+
+        internal void Display()
+        {
+            Console.WriteLine("Roll No: " + roll_no);
+            Console.WriteLine("Fees: " + fees);
+        }
+    }
+
+}
