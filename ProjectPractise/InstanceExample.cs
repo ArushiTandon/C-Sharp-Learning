2af58d448d2348ecd1e439e083daa09e33c863d8
Arushi Tandon <arushitandon007@gmail.com>
2026-05-27 16:52:48 +0530

    Instance no static example

---
+++ ProjectPractise/InstanceExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractise
+{
+    internal class InstanceExample
+    {
+        internal int x = 100; //global instance
+
+        internal void fun1()
+        {
+            int y = 200; //local instance
+        }
+    }
+}
--- ProjectPractise/Program.cs
+++ ProjectPractise/Program.cs
@@ -8,6 +8,11 @@
             Console.WriteLine(StaticExample.x);
             StaticExample.fun();
 
+            InstanceExample obj = new InstanceExample();
+            Console.WriteLine(obj.x);
+            obj.fun1();
+
+
         }
     }
 }
