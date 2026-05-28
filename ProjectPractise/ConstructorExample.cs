2d9a8bf239bcd1c8d1a13f274b96d6fd42e45948
Arushi Tandon <arushitandon007@gmail.com>
2026-05-28 20:49:17 +0530

    Static Constructor Example

---
+++ ProjectPractise/ConstructorExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPractise
+{
+    internal class ConstructorExample
+    {
+        static ConstructorExample()
+        {
+            Console.WriteLine("static Constructor");
+        }
+        internal ConstructorExample()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+
+        internal ConstructorExample(int x)
+        {
+            Console.WriteLine("Parameterized Constrcutor: " + x);
+        }
+    }
+}
--- ProjectPractise/Program.cs
+++ ProjectPractise/Program.cs
@@ -4,38 +4,17 @@ namespace ProjectPractise
 {
     internal class Program
     {
+        static Program()
+        {
+            Console.WriteLine("Static Constructor called");
+        }
+
        
         static void Main(string[] args)
         {
+            //ConstructorExample obj = new ConstructorExample();
+
           
-           Student[] obj = new Student[3];
-            for(int i = 0; i < obj.Length; i ++)
-            {
-                obj[i] = new Student();
-                Console.WriteLine("Enter the roll number of student " );
-                int r_no = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Enter the fees of student ");
-                int fees = Convert.ToInt32(Console.ReadLine());
-
-                obj[i].Accept(r_no , fees);
-            }
-
-            int max = 0;
-            int index = 0;
-            for( int i = 0; i < obj.Length; i++)
-            {
-                if (max < obj[i].fees)
-                {
-                    max = obj[i].fees;
-                    index = i;
-                }
-            }
-
-            Console.WriteLine("maximum fees is {0}", max);
-            obj[index].Display();
-
-
         }
     }
 }
